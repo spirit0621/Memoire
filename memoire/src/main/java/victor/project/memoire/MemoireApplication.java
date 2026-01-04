f9e126e9be4961758100fe9bf1e4eace8048820d
spirit0621 <alvesfernandes594@gmail.com>
2026-01-04 16:52:47 +0100

    Erreur_branche

--- memoire/src/main/java/victor/project/memoire/MemoireApplication.java
+++ memoire/src/main/java/victor/project/memoire/MemoireApplication.java
@@ -9,11 +9,66 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MemoireApplication {
-    public static void main(String[] args) {
-      SpringApplication.run(MemoireApplication.class, args);
+  public static void main(String[] args) {
+    SpringApplication.run(MemoireApplication.class, args);
+
+    Employee employee = new Employee("victor", 25, "BOSS");
+
+    
+    System.out.println(employee.getName());
+
+  }
+
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "param", defaultValue = "world") String name) {
+    String result;
+    /*
+     * if (name.equals ("victor")) {
+     * result="BOSS";
+     * }
+     * else if (name.equals("charles")){
+     * result="sensei";
+     * }
+     * else {
+     * result=name;
+     * }
+     */
+    switch (name) {
+      case "victor":
+        result = "BOSS";
+        break;
+      case "charles":
+        result = "sensei";
+        break;
+
+      default:
+        result = name;
+        break;
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+    System.out.println(result);
+    return String.format("Hello %s!", result);
+
+  }
+
+  @GetMapping("/countdown")
+  public String countdown(@RequestParam(value = "param", defaultValue = "4") int name) {
+
+    String result = "decompte";
+
+    System.out.println(name != 0);
+    {
+      String nbAjoute = String.valueOf(name);
+
+      result = result + nbAjoute;
+
+      name = name - 1;
+      System.out.println(result);
     }
+
+    while (name != 0);
+
+    return result;
+
+  }
+
 }
