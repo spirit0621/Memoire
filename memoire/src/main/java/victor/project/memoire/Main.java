ec4e46757b46c2eab1242c6bde0174e1e13b2d93
spirit0621 <alvesfernandes594@gmail.com>
2026-01-04 18:08:14 +0100

    1er exercice fait

---
+++ memoire/src/main/java/victor/project/memoire/Main.java
@@ -0,0 +1,11 @@
+package victor.project.memoire;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String texte = "Hello World";
+
+        System.out.println(texte);
+    }
+}
