0a021c0586dcddcc1ddc7ea6f47aac61429e7648
spirit0621 <alvesfernandes594@gmail.com>
2026-01-04 18:44:04 +0100

    Les exerices Partie 1 java

--- memoire/src/main/java/victor/project/memoire/Main.java
+++ memoire/src/main/java/victor/project/memoire/Main.java
@@ -1,11 +1,35 @@
 package victor.project.memoire;
 
-public class Main {
+// exerice 1
+//public class Main {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        String texte = "Hello World";
+//         String texte = "Hello World";
 
-        System.out.println(texte);
-    }
-}
+//         System.out.println(texte);
+//     }
+// }
+
+//exerice 2
+// public class Main {
+//     public static void affichage(String texte) {
+//         System.out.println("Bonjour " + texte);
+//     }
+
+//     public static void main(String[] args) {
+//         affichage("Marty");
+//     }
+// }
+// exerice 3
+// public class Main {
+// public static void main(String[] args) {
+//     int tableau[]={26, 10, 1985, 0, 12, 11, 1955};
+//     int cpt=0;
+//     for(int i=0; i<7; i++) {
+//        		if(tableau[i] == 0) {
+// 			cpt++;
+// 		}
+//     }System.out.println(cpt);
+// }
+// }
